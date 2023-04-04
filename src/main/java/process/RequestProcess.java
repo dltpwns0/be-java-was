e23c121d14906bae4af79af5a731c701dab875d0
CiRe <dltpwns6@naver.com>
2023-04-04 18:18:04 +0900

    Refactor : 메소드 분리를 통한 코드 수정
    기존의 코드를 조금은 보기 쉽게 하기위해 메소드를 분리하였다.

--- src/main/java/process/RequestProcess.java
+++ src/main/java/process/RequestProcess.java
@@ -13,6 +13,7 @@ public class RequestProcess {
 
     private final String rootPath = "src/main/resources/";
     private final String[] basePath = {"static", "templates"};
+    private final String welcomePage = "templates/index.html";
     private Head head;
 
     public byte[] process(String request) throws FileNotFoundException, IOException {
@@ -26,18 +27,22 @@ public class RequestProcess {
     }
 
     private byte[] getRequestFileAsByte() throws IOException {
-        String requestUrl = head.getUrl();
-        // 파일 존재하는지 체크
+        String requestUrl = head.getUrl().split("\\?")[0];
+
+        // TODO : 코드를 하나도 알아볼 수 없다. 수정 필요 (메소드 분리)
         Optional<File> fileOptional = Arrays.stream(basePath)
-                .filter(basePath -> {
-                    File f = new File(rootPath + basePath + requestUrl);
-                    return f.exists();
-                })
+                .filter(basePath -> findFile(requestUrl, basePath))
                 .findFirst()
                 .map(basePath -> new File((rootPath + basePath + requestUrl)));
-        if (fileOptional.isEmpty()) throw new FileNotFoundException();
-        File file = fileOptional.get();
+
+        // 요청 파일이 없다면 웰컴 페이지 리턴
+        File file = fileOptional.orElse(new File(rootPath + welcomePage));
         byte[] body = Files.readAllBytes(file.toPath());
         return body;
+    }
+
+    private boolean findFile(String requestUrl, String basePath) {
+        File f = new File(rootPath + basePath + requestUrl);
+        return f.exists();
     }
 }
