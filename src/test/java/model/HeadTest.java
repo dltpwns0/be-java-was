2030a4dd6f65f947a76504704e45c3efb1af6656
CiRe <dltpwns6@naver.com>
2023-04-06 18:13:29 +0900

    Test : 테스트 코드 변경

--- src/test/java/model/HeadTest.java
+++ src/test/java/model/HeadTest.java
@@ -3,19 +3,21 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.HttpMethod;
+
 import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HeadTest {
     StringBuilder sb;
-    HttpRequestHead head;
+    HttpRequest head;
     @BeforeEach
     public void beforeEach() throws IOException {
         sb = new StringBuilder();
 
     }
     @Test
-    @DisplayName("HttpRequestHead 클래스의 생성자가 String 타입의 헤더를 입력받아, 요청라인을 파싱하는지 테스트")
+    @DisplayName("HttpRequest 클래스의 생성자가 String 타입의 헤더를 입력받아, 요청라인을 파싱해야 한다.")
     public void HttpRequestLineTest() throws IOException {
         // when
         sb.append("GET /index.html?userId=cire&password=1234&name=이동준&email=dltpwns6@naver.com HTTP1.1").append("\n");
@@ -24,17 +26,17 @@ public class HeadTest {
         sb.append("\n");
 
         // given
-        head = new HttpRequestHead(sb.toString());
+        head = new HttpRequest(sb.toString());
 
         // then
-        assertThat(head.getMethod()).isEqualTo("GET");
+        assertThat(head.getMethod()).isEqualTo(HttpMethod.GET);
         assertThat(head.getUrl()).isEqualTo("/index.html");
         assertThat(head.getRequestParam()).isEqualTo("userId=cire&password=1234&name=이동준&email=dltpwns6@naver.com");
         assertThat(head.getVersion()).isEqualTo("HTTP1.1");
     }
 
     @Test
-    @DisplayName("HttpRequestHead 클래스의 생성자가 String 타입의 헤더를 입력받아, MIME 헤더를 파싱하는지 테스트")
+    @DisplayName("HttpRequest 클래스의 생성자가 String 타입의 헤더를 입력받아, 헤더를 파싱해야 한다.")
     public void HttpRequestMIMETest() throws IOException {
         // when
         sb.append("GET /index.html?userId=cire&password=1234&name=이동준&email=dltpwns6@naver.com HTTP1.1").append("\n");
@@ -43,7 +45,7 @@ public class HeadTest {
         sb.append("\n");
 
         // given
-        head = new HttpRequestHead(sb.toString());
+        head = new HttpRequest(sb.toString());
 
         // then
         assertThat(head.getHeaderElement("accept")).isEqualTo("*/*");
