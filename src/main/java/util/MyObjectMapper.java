bcd7c5e7c2cb4a48fa6d8bd1ee899778985bec26
CiRe <dltpwns6@naver.com>
2023-04-12 11:05:05 +0900

    Chore : 불필요한 주석 제거
    처음 ObjectMapper를 만든 목적은, User 클래스 객체를 만들기 위해서였습니다.
    
    현재 ObjectMapper 는 모든 클래스 객체를 만들 수 있도록 리펙토링을 하였습니다.

--- src/main/java/util/MyObjectMapper.java
+++ src/main/java/util/MyObjectMapper.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public class MyObjectMapper {
 
-    public static Optional<?> readValue(Object requestParams, Class<?> userClass) throws Exception {// 사실 userClass 를 입력 받을 필요는 없지만, 공부를 위해 넣어 봤다.
+    public static Optional<?> readValue(Object requestParams, Class<?> userClass) throws Exception {
         if (requestParams instanceof Map<?, ?>) {
             // TODO : 언체크 예외 처리를 해야하나?
             return MyObjectMapperByMap.readValue((Map<String, String>) requestParams, userClass);
