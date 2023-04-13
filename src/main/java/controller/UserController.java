cd76917300b8754b73c248112e95d58d0f9e4daf
CiRe <dltpwns6@naver.com>
2023-04-13 21:29:23 +0900

    Update : 회원가입 성공시, 상태코드 201 을 보내주도록 업데이트

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -5,31 +5,31 @@ import model.HttpRequest;
 import model.HttpResponse;
 import service.UserService;
 import type.RequestMethod;
+
 import java.util.Map;
 
 @RequestMapping(path = "/user")
-public class UserController implements Controller{
+public class UserController implements Controller {
 
     private final UserService userService = new UserService();
 
     @RequestMapping(path = "/create", method = RequestMethod.POST)
-    public void createAsPost(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+    public void create(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
         String requestUrl = httpRequest.getPathInfo();
         Map<String, String> requestParams = httpRequest.getRequestBody();
         userService.join(requestParams);
-        httpResponse.redirect("/");
-    }
-    @RequestMapping(path = "/create", method = RequestMethod.GET)
-    public void createAsGet(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
-        String requestUrl = httpRequest.getPathInfo();
-        String requestParams = httpRequest.getQueryString();
-        userService.join(requestParams);
-        httpResponse.redirect("/");
+        httpResponse.sendRedirect("/user/success-create-id");
     }
 
-    @RequestMapping
+    @RequestMapping()
     public void show(HttpRequest httpRequest, HttpResponse httpResponse) {
         String requestUrl = httpRequest.getPathInfo();
-        httpResponse.setPath(requestUrl);
+        httpResponse.addHeader("Request-URL", requestUrl);
+    }
+
+    // TODO  : 상태 코드를 바꿀려고, 이렇게 핸들러까지 추가 해야하나? (더 좋은 방법이 있을 것이다!)
+    @RequestMapping(path = "/success-create-id")
+    public void createSuccess(HttpRequest httpRequest, HttpResponse httpResponse) {
+        httpResponse.setStatus(HttpResponse.SC_CREATE);
     }
 }
