db02ebf74f2a927783229e1d52f0a4a8c811553d
CiRe <dltpwns6@naver.com>
2023-04-13 13:47:42 +0900

    Feat : 요청 url을 사용하여 서블릿을 찾는 기능 구현
    Http 요청을 보고, 요청을 처리할 수 있는 서블릿을 반환하는 기능을 구현하였습니다.

--- src/main/java/servlet/HttpServletContainer.java
+++ src/main/java/servlet/HttpServletContainer.java
@@ -1,24 +1,36 @@
 package servlet;
 
-import type.HttpMethod;
-import java.util.HashMap;
-import java.util.Map;
+import annotation.RequestMapping;
+import model.HttpRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpServletContainer {
 
-    private final Map<HttpMethod , HttpServlet> servletMap;
+    private final HttpServlet defaultServlet;
+    private final List<HttpServlet> servlets;
 
+    // TODO : 설정 파일로 초기화 할 수 있는 방법을 다시 생각해보자! (힌트 : https://lucas.codesquad.kr/masters-2023/course/backend-java/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%94%84%EB%A0%88%EC%9E%84%EC%9B%8C%ED%81%AC/Dependency-Injection)
     public HttpServletContainer() {
-        this.servletMap = new HashMap<>();
-        servletMap.put(HttpMethod.GET, new HttpGetServlet());
-        servletMap.put(HttpMethod.POST, new HttpPostServlet());
-
+        this.servlets = new ArrayList<>();
+        servlets.add(new HttpUserServlet());
+        this.defaultServlet = new HttpDefaultServlet();
     }
 
-    public HttpServlet getServlet(String method)  {
-        HttpMethod httpMethod = HttpMethod.getMethod(method);
+    public HttpServlet getServlet(HttpRequest httpRequest) {
+        String pathInfo = httpRequest.getPathInfo();
 
-        return servletMap.get(httpMethod);
+        for (HttpServlet servlet : servlets) {
+            RequestMapping requestAnnotation = servlet.getClass().getAnnotation(RequestMapping.class);
+
+            String[] servletPaths = requestAnnotation.path();
+            for (String servletPath : servletPaths) {
+                if (pathInfo.startsWith(servletPath)) {
+                    return servlet;
+                }
+            }
+        }
+        return defaultServlet;
     }
-
 }
