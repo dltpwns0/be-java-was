dd48e31c327f30e61be8804a97b00591d6c82108
CiRe <dltpwns6@naver.com>
2023-04-20 00:48:32 +0900

    Feat : Article 클래스 추가
    이전 스프링 미션에서 구현했던 Article 클래스를 가져왔다.

---
+++ src/main/java/model/Article.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+
+public class Article {
+    private int articleId;
+    private String writer;
+    private String title;
+    private String contents;
+
+    private Timestamp timeStamp;
+
+    public Article() {
+    }
+
+    public Article(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Article(int articleId, String writer, String title, String contents) {
+        this.articleId = articleId;
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Article(Article article) {
+        this.articleId = article.articleId;
+        this.writer = article.writer;
+        this.title = article.title;
+        this.contents = article.contents;
+        this.timeStamp = article.timeStamp;
+    }
+
+    public int getArticleId() { return articleId; }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getFormattedTime() {
+        return timeStamp.toLocalDateTime().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
+
+    public void setTimeStamp(Timestamp timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
