b2691e0c57d89bf77c8b97cf3cf5d08f151601bc
CiRe <dltpwns6@naver.com>
2023-04-20 21:14:59 +0900

    Feat : ArgumentResolver 기능 구현
    핸들러의 파라미터를 보고, 필요한 파라미터를 List 자료형을 반환하는 기능을 구현하였다.

---
+++ src/main/java/util/MyArgumentResolver.java
@@ -0,0 +1,83 @@
+package util;
+
+import annotation.PathVariable;
+import annotation.RequestMapping;
+import controller.Controller;
+import servlet.HttpRequest;
+import servlet.HttpResponse;
+import view.ModelAndView;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArgumentResolver {
+
+
+    public List<Object> resolve(HttpRequest httpRequest, HttpResponse httpResponse, ModelAndView modelAndView, MyHandlerMethod handler) {
+        Controller controller = handler.getHandler();
+        Method method = handler.getMethod();
+
+        Parameter[] parameters = method.getParameters();
+
+        List<Object> arguments = new ArrayList<>();
+
+        // TODO : 이렇게 조건문 투성이로 하지말고, 다른 방법은 없을까?
+        for (Parameter parameter : parameters) {
+            PathVariable parameterAnnotation = parameter.getAnnotation(PathVariable.class);
+            Class<?> parameterType = parameter.getType();
+
+            if (parameterAnnotation == null) {
+                if (parameterType == HttpRequest.class) {
+                    arguments.add(httpRequest);
+                    continue;
+                }
+                if (parameterType == HttpResponse.class) {
+                    arguments.add(httpResponse);
+                    continue;
+                }
+                if (parameterType == ModelAndView.class) {
+                    arguments.add(modelAndView);
+                    continue;
+                }
+            }
+
+            String[] handlerPathInfos = getHandlerPaths(handler);
+            String[] requestPathInfos = httpRequest.getPathInfo().split("/");
+
+            String parameterPathName ="{" + parameterAnnotation.name() + "}";
+
+            for (int i = 0; i < handlerPathInfos.length; i++) {
+                if (handlerPathInfos[i].equals(parameterPathName)) {
+                    arguments.add(convertType(parameterType, requestPathInfos[i]));
+                    break;
+                }
+            }
+        }
+        return arguments;
+    }
+
+    private String[] getHandlerPaths(MyHandlerMethod handlerMethod) {
+        Controller controller = handlerMethod.getHandler();
+        Method method = handlerMethod.getMethod();
+
+        String controllerAnnotationPathInfo = controller.getClass().getAnnotation(RequestMapping.class).path()[0];
+        String methodAnnotationPathInfos = method.getAnnotation(RequestMapping.class).path()[0];
+
+        String pathInfos = controllerAnnotationPathInfo + methodAnnotationPathInfos;
+
+        return pathInfos.split("/");
+    }
+
+    private static Object convertType(Class<?> clazz, String value) {
+        Object object = null;
+        if (clazz == String.class) {
+            return value;
+        }
+        if (clazz == int.class || clazz == Integer.class) {
+            return Integer.parseInt(value);
+        }
+        return null;
+    }
+}
