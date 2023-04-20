8e0c927f600b21543823ace37f1803e914e512a8
CiRe <dltpwns6@naver.com>
2023-04-20 13:49:02 +0900

    테스트용 : 서버 데이터 베이스를 일정하게으로 유지하기 위한 코드 추가

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,7 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import configure.AppConfiguration;
 import configure.ApplicationContext;
@@ -12,9 +19,27 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
+    private static final String URL = "jdbc:mysql://127.0.01:3306/codesquad";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+
+
     public static void main(String args[]) throws Exception {
         AppConfiguration appConfigure = new AppConfiguration();
         ApplicationContext applicationContext = new ApplicationContext(appConfigure);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            executeSqlFile("src/schema.sql", connection);
+            executeSqlFile("src/data.sql", connection);
+
+            connection.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
         int port = 0;
         if (args == null || args.length == 0) {
@@ -35,4 +60,34 @@ public class WebServer {
             }
         }
     }
+
+    private static void executeSqlFile(String filePath, Connection connection) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("--") || line.trim().isEmpty()) {
+                    continue;
+                }
+
+                sb.append(line);
+
+                if (line.endsWith(";")) {
+                    try (Statement statement = connection.createStatement()) {
+                        statement.execute(sb.toString());
+                    } catch (SQLException e) {
+                        System.err.println("Error executing SQL statement: " + sb.toString());
+                        e.printStackTrace();
+                    }
+
+                    sb.setLength(0);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + filePath);
+            e.printStackTrace();
+        }
+    }
+
 }
