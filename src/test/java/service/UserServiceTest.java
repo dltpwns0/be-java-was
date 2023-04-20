5c2dedb3a5bafbb140835203b38615de4cd26c9e
CiRe <dltpwns6@naver.com>
2023-04-20 21:22:30 +0900

    Test : 유저테스트 코드 변경
    데이터베이스를 사용하도록 코드드 수정하였다.

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,15 +1,28 @@
 package service;
 
+import db.ArticleDatabase;
 import db.Database;
+import db.UserDatabase;
 import model.User;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserServiceTest {
 
-    UserService userService = new UserService();
+    private final String URL = "jdbc:mysql://127.0.01:3306/codesquad";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "1234";
 
+    UserDatabase userDatabase;
+    UserService userService;
+
+    @BeforeEach
+    public void beforeEach() {
+        userDatabase = new UserDatabase(URL, USERNAME, PASSWORD);
+        userService = new UserService(userDatabase);
+    }
 
     @Test
     @DisplayName("요청 파라미터를 인자를 받으면, 데이터베이스에 회원 정보가 저장되어야 한다.")
@@ -20,7 +33,7 @@ class UserServiceTest {
 
         // when
         userService.join(requestParams);
-        User reultUser = Database.findUserById("cire");
+        User reultUser = userDatabase.findUserById("cire");
         User expectUser = new User("cire", "1234", "이동준", "dltpwns6@naver.com");
 
         Assertions.assertThat(reultUser).usingRecursiveComparison().isEqualTo(expectUser);
