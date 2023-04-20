9a31a46cba695e31f6e3fb2c0162e898d0677783
CiRe <dltpwns6@naver.com>
2023-04-20 12:39:39 +0900

    Config : 라이브러리 다운 및 sql 파일 작성

---
+++ src/schema.sql
@@ -0,0 +1,21 @@
+drop table if exists ARTICLES;
+drop table if exists USERS;
+
+create table USERS
+(
+    userId   varchar(20) not null,
+    password varchar(20) not null,
+    name     varchar(20) not null,
+    email    varchar(20) not null,
+    primary key (userId)
+);
+create table ARTICLES
+(
+    articleId int auto_increment,
+    writer    varchar(20) not null,
+    title     TINYTEXT    not null,
+    contents  TEXT        not null,
+    timeStamp TIMESTAMP default current_timestamp,
+    primary key (articleId)
+);
+
